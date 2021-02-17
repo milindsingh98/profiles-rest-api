203baa80d661c0c1d5c0e1e43fad254ff4c6702f
MILIND SINGH DEV <milindsingh98@gmail.com>
2021-02-17 16:59:48 +0530

    added the put deleted and patch methods

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,11 +1,15 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
+from profiles_api import serializers
 
 class HelloAPIView(APIView):
     '''
     Test API view
     '''
+
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         '''
@@ -19,3 +23,40 @@ class HelloAPIView(APIView):
         ]
 
         return Response({'message' : 'Hello!', 'an_apiview' : an_apiview})
+
+    def post(self, request):
+        '''
+        Create a helloe request with our name
+        '''
+
+        serializer = self.serializer_class(data = request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+
+            return Response({'message' : message})
+
+        else:
+            return Response(
+                serializer.errors,
+                status = status.HTTP_400_BAD_REQUEST
+                ) 
+
+    def put(self, request, pk= None):
+        '''
+        handling updating an object
+        '''
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk= None):
+        '''
+        Handle a partial update of an object
+        '''
+        return Response({'method':'PATCH'})
+
+    def delete(self, request, pk = None):
+        '''
+        Delete the object 
+        '''
+        return Response({'method': 'DELETE'})
